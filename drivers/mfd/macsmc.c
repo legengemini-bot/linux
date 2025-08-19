08e6e7df3be5db1f4a6e43c6217ddb9a4d1bbf9e
James Calligeros <jcalligeros99@gmail.com>
2025-08-19 21:47:56 +1000

    hwmon: Add Apple Silicon SMC hwmon driver
    The System Management Controller on Apple Silicon devices is responsible
    for integrating and exposing the data reported by the vast array of
    hardware monitoring sensors present on these devices. It is also
    responsible for fan control, and allows users to manually set fan
    speeds if they so desire. Add a hwmon driver to expose current,
    power, temperature, and voltage monitoring sensors, as well as
    fan speed monitoring and control via the SMC on Apple Silicon devices.
    
    The SMC firmware has no consistency between devices, even when they
    share an SoC. The FourCC keys used to access sensors are almost
    random. An M1 Mac mini will have different FourCCs for its CPU core
    temperature sensors to an M1 MacBook Pro, for example. For this
    reason, the valid sensors for a given device are specified in a
    child of the SMC Devicetree node. The driver uses this information
    to determine which sensors to make available at runtime.
    
    Co-developed-by: Janne Grunau <j@jannau.net>
    Signed-off-by: Janne Grunau <j@jannau.net>
    Signed-off-by: James Calligeros <jcalligeros99@gmail.com>

--- drivers/mfd/macsmc.c
+++ drivers/mfd/macsmc.c
@@ -48,6 +48,7 @@ static const struct mfd_cell apple_smc_devs[] = {
 	MFD_CELL_OF("macsmc-gpio", NULL, NULL, 0, 0, "apple,smc-gpio"),
 	MFD_CELL_OF("macsmc-reboot", NULL, NULL, 0, 0, "apple,smc-reboot"),
 	MFD_CELL_OF("macsmc-rtc", NULL, NULL, 0, 0, "apple,smc-rtc"),
+	MFD_CELL_OF("macsmc_hwmon", NULL, NULL, 0, 0, "apple,smc-hwmon"),
 };
 
 static int apple_smc_cmd_locked(struct apple_smc *smc, u64 cmd, u64 arg,
